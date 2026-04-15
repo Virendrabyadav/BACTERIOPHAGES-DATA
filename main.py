aac417e09d463a0726fba6565e957cdc73706416
Virendrabyadav <97837630+Virendrabyadav@users.noreply.github.com>
2026-04-15 18:37:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
-# In-memory database (temporary)
-phages = []
-requests = []
-
-class Phage(BaseModel):
-    name: str
-    host_bacteria: str
-    source: str
-    lytic: bool
-    lab: str
-
-class Request(BaseModel):
-    clinician: str
-    bacteria: str
-    urgency: str
-
-@app.get("/")
-def home():
-    return {"message": "Phage Directory API Running"}
-
-@app.post("/add_phage")
-def add_phage(phage: Phage):
-    phages.append(phage)
-    return {"message": "Phage added successfully"}
-
-@app.get("/list_phages")
-def list_phages():
-    return phages
-
-@app.post("/request_phage")
-def request_phage(req: Request):
-    matched = [p for p in phages if p.host_bacteria.lower() == req.bacteria.lower()]
-    response = {
-        "request": req,
-        "matches": matched
-    }
-    requests.append(response)
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
